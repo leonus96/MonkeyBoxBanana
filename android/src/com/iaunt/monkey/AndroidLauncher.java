5714a2f0283ac1c43968d447f72e9d7da1323cc8
Joseph León <leonus96@gmail.com>
2018-05-01 11:17:34 -0500

    first commit

---
+++ android/src/com/iaunt/monkey/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.iaunt.monkey;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.iaunt.monkey.MonkeyBoxBanana;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MonkeyBoxBanana(), config);
+	}
+}
---
+++ desktop/src/com/iaunt/monkey/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.iaunt.monkey.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.iaunt.monkey.MonkeyBoxBanana;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MonkeyBoxBanana(), config);
+	}
+}
